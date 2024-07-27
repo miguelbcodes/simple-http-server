1f1518e58fcb7437319943d11a748fba2e81ff33
Miguel Bertol <miguelbertolcodes@gmail.com>
2024-07-27 10:35:31 -0300

    feat(server): add accept_connection declaration in server.h

--- include/server.h
+++ include/server.h
@@ -4,9 +4,11 @@
 #include <netinet/in.h>
 
 #define PORT 8080
+#define BUFFER_SIZE 1024
 
 int create_socket();
 void bind_socket(int server_fd, struct sockaddr_in *address);
 void listen_socket(int server_fd);
+void accept_connection(int server_fd);
 
 #endif
