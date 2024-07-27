4780b9d5a2a391a74879883eadfc12a9aea7fad8
Miguel Bertol <miguelbertolcodes@gmail.com>
2024-07-27 11:41:53 -0300

    feat(server): add http_request struct in server.h

--- include/server.h
+++ include/server.h
@@ -6,11 +6,17 @@
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
+typedef struct {
+  char method[16];
+  char url[256];
+  char protocol[16];
+} http_request;
+
 int create_socket();
 void bind_socket(int server_fd, struct sockaddr_in *address);
 void listen_socket(int server_fd);
 void accept_connection(int server_fd);
-void parse_http_request(const char *request);
+void parse_http_request(const char *request, http_request *parsed_request);
 void generate_response(int client_socket, const char *status, const char *content_type, const char *body);
 void serve_static_file(int client_socket, const char *path);
 
