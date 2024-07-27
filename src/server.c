2d9a1f27bd32f58b596b0da47484e226f2b1b8ac
Miguel Bertol <miguelbertolcodes@gmail.com>
2024-07-27 00:49:15 -0300

    feat(server): implement create_socket function in server.c

---
+++ src/server.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include "server.h"
+
+int create_socket() {
+  int server_fd;
+  int opt = 1;
+
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0) == 0)) {
+    perror("socket failed");
+    exit(EXIT_FAILURE);
+  }
+
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+    perror("setsockopt");
+    close(server_fd);
+    exit(EXIT_FAILURE);
+  }
+
+  return server_fd;
+}
